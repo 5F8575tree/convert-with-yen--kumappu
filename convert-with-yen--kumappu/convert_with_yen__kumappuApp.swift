b28d0b65669ac9820b455588e84ca10f6a0c71cf
Mark J Rawlins <96557207+5F8575tree@users.noreply.github.com>
2024-03-05 17:12:56 +0900

    Initial Commit

---
+++ convert-with-yen--kumappu/convert_with_yen__kumappuApp.swift
@@ -0,0 +1,17 @@
+//
+//  convert_with_yen__kumappuApp.swift
+//  convert-with-yen--kumappu
+//
+//  Created by Mark J Rawlins on 2024/03/05.
+//
+
+import SwiftUI
+
+@main
+struct convert_with_yen__kumappuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
